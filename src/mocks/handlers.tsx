b6ca17db46e4c84896b0829d192ca903f9ed5987
Austin Layman <61898015+EnkiEnki77@users.noreply.github.com>
2023-01-15 05:33:19 -0800

    setup project

---
+++ src/mocks/handlers.tsx
@@ -0,0 +1,21 @@
+import { rest } from "msw";
+
+export const handlers = [
+  rest.get("http://localhost:3030/scoops", (req, res, ctx) => {
+    return res(
+      ctx.json([
+        { name: "Chocolate", imagePath: "/images/chocolate.png" },
+        { name: "Vanilla", imagePath: "/images/vanilla.png" },
+      ])
+    );
+  }),
+  rest.get("http://localhost:3030/toppings", (req, res, ctx) => {
+    return res(
+      ctx.json([
+        { name: "Hot fudge", imagePath: "/images/hot-fudge.png" },
+        { name: "Peanut butter", imagePath: "/images/peanut-butter" },
+        { name: "Gummi bears", imagePath: "/images/gummi-bears.png" },
+      ])
+    );
+  }),
+];
